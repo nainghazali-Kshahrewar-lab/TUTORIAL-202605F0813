f6016098e7c92df3a120942d3da71e319975fa1b
nainghazali-Kshahrewar-lab <nainghazali26@gmail.com>
2026-07-08 16:14:24 +0800

    Update food_order.py

--- week_7/food_order.py
+++ week_7/food_order.py
@@ -1,1 +1,8 @@
-
+def calculate_total(price, quantity):
+  
+    if price <= 0:
+        return "invalid price"
+    if quantity <= 0:
+        return "invalid quantity"
+      
+    return price * quantity
